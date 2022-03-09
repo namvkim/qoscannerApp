300fb487ced84a051a9d8cfb7353bda5feea5ee2
hothihang <hang.ho22@student.passerellesnumeriques.org>
2022-03-09 11:16:29 +0700

    fix version firebase

--- service.js
+++ service.js
@@ -1,11 +1,12 @@
 import { doc, getDoc, collection, onSnapshot } from "firebase/firestore";
 import { db } from "./firebase";
 
-const citiesRef = collection(db, "restaurant");
-const getAllMenu = (res) => {
-  onSnapshot(doc(db, "restaurant", "SF"), (doc) => {
-    console.log("Current data: ", doc.data());
-  });
-};
+// const citiesRef = collection(db, "restaurant");
+const citiesRef = db;
+// const getAllMenu = (res) => {
+//   onSnapshot(doc(db, "restaurant", "SF"), (doc) => {
+//     console.log("Current data: ", doc.data());
+//   });
+// };
 
 export { citiesRef };
